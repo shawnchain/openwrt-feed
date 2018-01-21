4c93fc3935e81f0ef84d1a645bbb7df8a924218b
Shawn Chain <shawn.chain@gmail.com>
2018-01-21 22:40:03 +0800

    Add simple luci items

---
+++ mmdvm-host/files/usr/lib/lua/luci/controller/mmdvm/mmdvm.lua
@@ -0,0 +1,69 @@
+module("luci.controller.mmdvm.mmdvm", package.seeall)
+
+function index()
+        if not nixio.fs.access("/etc/config/mmdvm") then
+                return
+        end
+
+        local _mod
+        entry({"admin", "mmdvm"}, firstchild(), "MMDVM", 60).dependent=false
+        _mod = entry({"admin", "mmdvm", "log"}, call("mmdvm_log_action"), _("MMDVM Log") , 1)
+        _mod.sysauth = "root"
+        _mod.sysauth_authenticator = "htmlauth"
+
+        _mod = entry({"admin", "mmdvm", "ysfgwlog"}, call("ysfgw_log_action"), _("YSF Gateway Log"), 10)
+        _mod.sysauth = "root"
+        _mod.sysauth_authenticator = "htmlauth"
+
+        _mod = entry({"admin", "mmdvm", "ysfrllog"}, call("ysfrl_log_action"), _("YSF Reflector Log"), 20)
+        _mod.sysauth = "root"
+        _mod.sysauth_authenticator = "htmlauth"
+
+        _mod = entry({"admin", "mmdvm", "p25gwlog"}, call("p25gw_log_action"), _("P25 Gateway Log"), 30)
+        _mod.sysauth = "root"
+        _mod.sysauth_authenticator = "htmlauth"
+
+        _mod = entry({"admin", "mmdvm", "p25rllog"}, call("p25rl_log_action"), _("P25 Reflector Log"), 40)
+        _mod.sysauth = "root"
+        _mod.sysauth_authenticator = "htmlauth"
+end
+
+function mmdvm_log_action()
+        luci.http.prepare_content("text/plain")
+        luci.http.write("-= MMDVMHost Log =-")
+        for aline in luci.util.execi("_log=/var/log/MMDVM-`date -I`.log;[ -f $_log ] && tail -n 10 $_log || echo \"No log file $_log\"") do
+                luci.http.write("\n" .. aline)
+        end
+end
+
+function ysfgw_log_action()
+        luci.http.prepare_content("text/plain")
+        luci.http.write("-= YSF Gateway Log =-")
+        for aline in luci.util.execi("_log=/var/log/YSFGateway-`date -I`.log;[ -f $_log ] && tail -n 10 $_log || echo \"No log file $_log\"") do
+                luci.http.write("\n" .. aline)
+        end
+end
+
+function ysfrl_log_action()
+        luci.http.prepare_content("text/plain")
+        luci.http.write("-= YSF Reflector Log =-")
+        for aline in luci.util.execi("_log=/var/log/YSFReflector-`date -I`.log;[ -f $_log ] && tail -n 10 $_log || echo \"No log file $_log\"") do
+                luci.http.write("\n" .. aline)
+        end
+end
+
+function p25gw_log_action()
+        luci.http.prepare_content("text/plain")
+        luci.http.write("-= P25 Gateway Log =-")
+        for aline in luci.util.execi("_log=/var/log/P25Gateway-`date -I`.log;[ -f $_log ] && tail -n 10 $_log || echo \"No log file $_log\"") do
+                luci.http.write("\n" .. aline)
+        end
+end
+
+function p25rl_log_action()
+        luci.http.prepare_content("text/plain")
+        luci.http.write("-= P25 Reflector Log =-")
+        for aline in luci.util.execi("_log=/var/log/P25Reflector-`date -I`.log;[ -f $_log ] && tail -n 10 $_log || echo \"No log file $_log\"") do
+                luci.http.write("\n" .. aline)
+        end
+end
